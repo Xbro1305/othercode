560c405d4da93157bff655dfaf31560dda262ca6
Xbro1305 <x.bro2010@gmail.com>
2024-11-20 19:40:22 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Pages/Home/Home";
 import { Projects } from "./Pages/Projects/Projects";
+import { Services } from "./Pages/Services/Services";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/services" element={<Services />} />
     </Routes>
   );
 };
